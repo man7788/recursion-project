32fd130b7577db6e23dfc2b30f3bb46ec8aa88d0
man7788 <jerman1841@gmail.com>
2023-01-21 03:14:52 +0800

    Add merge sort numbers are duplicate or not

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -1,9 +1,18 @@
-function mergeSort(list, array = [], array2 = [], split = true, i = 0) {
-  // if (i > 50) {
-  //   console.log(i);
-  //   return array2;
-  // }
-
+function mergeSort(
+  list,
+  array = [],
+  array2 = [],
+  split = true,
+  i = 0,
+  j = 0,
+  k = 0,
+  merged = []
+) {
+  if (i > 50) {
+    console.log(i);
+    return array2;
+  }
+  // Split raw number list into two halves
   if (list.length > 0 && array.length === 0) {
     const left = list.slice(0, list.length / 2);
     const right = list.slice(list.length / 2, list.length);
@@ -12,6 +21,7 @@ function mergeSort(list, array = [], array2 = [], split = true, i = 0) {
     return mergeSort(list, array);
   }
 
+  // Continue spliting until all lists have only 1 number
   if (array.length > 0 && split === true) {
     if (array.length === list.length) {
       split = false;
@@ -32,89 +42,107 @@ function mergeSort(list, array = [], array2 = [], split = true, i = 0) {
     return mergeSort(list, array, array2, split);
   }
 
+  // Base case to return sorted list
   if (array2[0] !== undefined) {
     if (array2[0].length === list.length) {
       return array2[0];
     }
   }
 
+  // Starts merging process
   if (array.length > 0) {
+    // Finishes merging all pairs in one branch, starts again with new branch
     if (array[i] === undefined) {
       i = 0;
       array = array2;
       array2 = [];
       return mergeSort(list, array, array2, split, i);
     }
-
-    let merged = [];
-    if (array[i] !== undefined) {
-      if (array[i + 1] !== undefined) {
-        array[i].forEach((x) => {
-          array[i + 1].forEach((y) => {
-            // This sorting method doesn't work if numbers are duplicated
-            if (x < y && !merged.includes(x)) {
-              merged.push(x);
-            } else if (y < x && !merged.includes(y)) {
-              merged.push(y);
-            }
-          });
-        });
-
-        array[i].forEach((x) => {
-          if (!merged.includes(x)) {
-            merged.push(x);
-          }
-        });
-
-        array[i + 1].forEach((x) => {
-          if (!merged.includes(x)) {
-            merged.push(x);
-          }
-        });
-        array2.push(merged);
-      }
-
-      if (array[i + 1] === undefined) {
-        array2[array2.length - 1].forEach((x) => {
-          if (x < array[i] && !merged.includes(x)) {
-            merged.push(x);
-          } else if (x > array[i] && !merged.includes(x)) {
-            merged.push(array[i][0]);
-          }
-        });
-        array2[array2.length - 1].forEach((x) => {
-          if (!merged.includes(x)) {
-            merged.push(x);
-          }
-        });
-        array[i].forEach((x) => {
-          if (!merged.includes(x)) {
-            merged.push(x);
-          }
-        });
-
-        array2[array2.length - 1] = merged;
-      }
+    // No pair, even number list
+    if (array[i + 1] === undefined && array[array.length - 1].length > 1) {
+      i = 0;
+      j = 0;
+      k = 0;
+      array2.push(array[array.length - 1]);
+      array = array2;
       console.log(array2);
+      array2 = [];
+      return mergeSort(list, array, array2, split, i, j, k, merged);
     }
-    return mergeSort(list, array, array2, split, i + 2);
+
+    // No pair, odd number list
+    if (array[i + 1] === undefined && array[array.length - 1].length === 1) {
+      let lastIndex = array2[array2.length - 1];
+      const larger = lastIndex.filter((num) => num > array[i][0]);
+      const smaller = lastIndex.filter((num) => num < array[i][0]);
+      array2[array2.length - 1] = smaller.concat(array[i][0], larger);
+      console.log(array2);
+      i = 0;
+      array = array2;
+      array2 = [];
+      return mergeSort(list, array, array2, split, i, j, k, merged);
+    }
+    // Finishes merging one pair of lists, go to next pair
+    if (array[i][j] === undefined) {
+      console.log('j undefined');
+      const remains = array[i + 1].filter(
+        (num) => num > merged[merged.length - 1]
+      );
+
+      remains.forEach((num) => {
+        merged.push(num);
+      });
+      array2.push(merged);
+      console.log(array2);
+      i += 2;
+      j = 0;
+      k = 0;
+      return mergeSort(list, array, array2, split, i, j, k);
+    }
+    // Finishes merging one pair of lists, go to next pair
+    if (array[i + 1][k] === undefined) {
+      console.log(array[i + 1][k]);
+      console.log('k undefined');
+      let remains = array[i].filter((num) => num > merged[merged.length - 1]);
+      console.log(remains);
+      remains.forEach((num) => {
+        merged.push(num);
+      });
+      array2.push(merged);
+      console.log(array2);
+      i += 2;
+      j = 0;
+      k = 0;
+      return mergeSort(list, array, array2, split, i, j, k);
+    }
+
+    if (array[i + 1] !== undefined) {
+      // Merging two pairs of lists
+      if (array[i][j] < array[i + 1][k]) {
+        console.log(array[i][j]);
+        merged.push(array[i][j]);
+        j += 1;
+        console.log(merged, `${j}j`);
+        return mergeSort(list, array, array2, split, i, j, k, merged);
+      }
+      if (array[i][j] > array[i + 1][k]) {
+        console.log(array[i + 1][k]);
+        merged.push(array[i + 1][k]);
+        k += 1;
+        console.log(merged, `${k}k`);
+        return mergeSort(list, array, array2, split, i, j, k, merged);
+      }
+      if (array[i][j] === array[i + 1][k]) {
+        merged.push(array[i][j], array[i + 1][k]);
+        j += 1;
+        k += 1;
+        console.log(merged, j, k);
+        return mergeSort(list, array, array2, split, i, j, k, merged);
+      }
+    }
   }
 }
 export default mergeSort;
 
-// Make numbers in list compare to each other,
-// regardless of how many of them.
-
-// if num[0] < num[i]
-// add num[0] to list
-// else num[i+1] // Use recursion to return i+1 and list
-
-// Split list into two halves,
-// base case to start merging if list only have one number.
-// Run spliting in parallel left right.
-
-// if list length > 0
-// slice list (0, length / 2)
-// slice list (length / 2, 0)
-// if list length === 1
-// merge lists
+// [2, 13, 15, 18]
+// [4, 9, 14, 16, 17]
